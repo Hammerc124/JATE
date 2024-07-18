e2c1799dcd348dae3ae149dbb78a8a9bfbde5d2f
hammerc124 <hammer.calebr@gmail.com>
2024-07-18 11:17:05 -0400

    removed develop directory as it was causing problems

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,6 @@
+// import path package to use path.join
 const path = require('path');
-
+// export function to serve index.html
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
